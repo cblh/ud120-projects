1c4840d9312461a83ce40e046b565c12115ccaa1
burlinchen <905850648@qq.com>
2017-05-08 09:02:15 +0800

    add enron dataset questions

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -18,23 +18,43 @@
 import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
+print "How many data points (people) are in the dataset?(146) ", \
+        len(enron_data.keys())
 
-print len(enron_data)
-print enron_data['ALLEN PHILLIP K']['poi']
-print enron_data['ALLEN PHILLIP K']
-print len(enron_data['ALLEN PHILLIP K'])
+print "For each person, how many features are available?(21) ", \
+        len(enron_data[enron_data.keys()[0]].keys())
 
-keys = enron_data.keys()
-print keys
+
 poi_data = filter(
     lambda x:
     enron_data[x]['poi'],
     enron_data)
+print "How many POIs are there in the E+F dataset?(18) ", \
+    len(poi_data)
 
-print poi_data
+import sys
+sys.path.append("../final_project/")
 
-print 'len(poi_data)'
-print len(poi_data)
+
+poi_count = 0
+with open("../final_project/poi_names.txt") as names_file:
+    for line in names_file:
+        if line.startswith('(y)') or line.startswith('(n)') :
+            poi_count += 1
+print "How many POIs are there in the E+F dataset?(35) ", \
+    poi_count
+
+# filter_poi_data = filter(
+#     lambda x:
+#     operator.indexOf(poi_emails, poi_data[x]['email_address']),
+#     poi_data
+# )
+# filter_poi_data = reduce(
+#     lambda x:
+#     poi_data[x]['email_address'],
+#     poi_data
+# )
+# print len(filter_poi_data)
 import os
 
 os.system('say "your program has finished"')
