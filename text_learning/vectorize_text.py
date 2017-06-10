e243ea5bcd0a665ad62bb485edb5f74f4bf2c63d
burlinchen <905850648@qq.com>
2017-06-10 09:09:18 +0800

    词袋

--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -41,25 +41,33 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
     for path in from_person:
         ### only look at first 200 emails when developing
         ### once everything is working, remove this line to run over full dataset
-        temp_counter += 1
-        if temp_counter < 200:
-            path = os.path.join('..', path[:-1])
-            print path
-            email = open(path, "r")
+        #temp_counter += 1
+        #if temp_counter < 200:
+        path = os.path.join('..', path[:-1])
+        print path
+        email = open(path, "r")
 
-            ### use parseOutText to extract the text from the opened email
+        ### use parseOutText to extract the text from the opened email
+        text = parseOutText(email)
 
-            ### use str.replace() to remove any instances of the words
-            ### ["sara", "shackleton", "chris", "germani"]
+        ### use str.replace() to remove any instances of the words
+        ### ["sara", "shackleton", "chris", "germani"]
+        stopwords = ["sara", "shackleton", "chris", "germani", "sshacklensf", "cgermannsf"]
+        for word in stopwords:
+            text = text.replace(word, "")
+        ### append the text to word_data
+        word_data.append(text)
+        ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
+        if name=="sara":
+            from_data.append(0)
+        else:
+            from_data.append(1)
+        email.close()
 
-            ### append the text to word_data
-
-            ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
-
-
-            email.close()
+        email.close()
 
 print "emails processed"
+print "word data:", word_data[152]
 from_sara.close()
 from_chris.close()
 
@@ -71,5 +79,14 @@ pickle.dump( from_data, open("your_email_authors.pkl", "w") )
 
 
 ### in Part 4, do TfIdf vectorization here
+# english_sw = stopwords("English")
+from sklearn.feature_extraction.text import TfidfVectorizer
+from nltk.corpus import stopwords
 
+vectorizer = TfidfVectorizer(stop_words="english")
+vectorizer.fit(word_data)
+vectorizer.transform(word_data)
+feature_words = vectorizer.get_feature_names()
+print "number of words:", len(feature_words)
+print "word number 34597:", feature_words[34597]
 
